f93a10d0453e52e031d5c8e48cb871076eca953e
Vikas Prajapati <vik.prj5@gmail.com>
2026-07-17 12:27:09 +0530

    Add module to print star pattern
    This module prints a star pattern in a triangular format, with each row containing an increasing number of stars.

---
+++ star_pattern1.v
@@ -0,0 +1,23 @@
+/*
+
+Print Below Star Pattern
+* 
+** 
+*** 
+**** 
+*****
+
+*/
+module tb;
+  
+  integer i,j;
+  
+  initial begin
+    for(i = 1; i <= 5; i = i + 1) begin
+      for(j = 1; j <= i; j = j + 1) begin
+        $write("*");
+      end
+      $display();
+    end
+  end
+endmodule
